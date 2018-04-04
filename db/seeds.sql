e57fca1d20dd7dcd69ebf107ff16adb4080b85ed
Chris Clark <christopheryc94@gmail.com>
2018-04-03 20:32:44 -0700

    working application with styling

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Western BBQ');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheddar');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Parmasean Sourdough Melt', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', true);
+INSERT INTO burgers (burger_name) VALUES ("Bob's Big Bear Burger");
